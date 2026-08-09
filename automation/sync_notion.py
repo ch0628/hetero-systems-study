c57b6f7754e73ff27ed640e61138ba3609143297
ch0628 <tkiwi@daum.net>
2026-08-09 12:56:55 +0900

    error fixed

--- automation/sync_notion.py
+++ automation/sync_notion.py
@@ -211,7 +211,9 @@ def main() -> None:
         raise RuntimeError("NOTION_TOKEN이 없습니다.")
 
     page_env = (
-e
+        "NOTION_CPU_PAGE_ID"
+    if args.target == "cpu"
+    else "NOTION_GPU_PAGE_ID"
     )
 
     page_id = os.environ.get(page_env)
